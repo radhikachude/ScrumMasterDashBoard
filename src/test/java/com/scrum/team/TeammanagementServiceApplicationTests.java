2082aa05c0d76cffc1483fcf09b82ffabe003363
radhikachude <76487409+radhikachude@users.noreply.github.com>
2024-12-29 22:37:29 +0530

    Update TeammanagementServiceApplicationTests.java

--- src/test/java/com/scrum/team/TeammanagementServiceApplicationTests.java
+++ src/test/java/com/scrum/team/TeammanagementServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.scrum.team;
+// package com.scrum.team;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TeammanagementServiceApplicationTests {
+// @SpringBootTest
+// class TeammanagementServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
